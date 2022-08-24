3f5fb4544ed2bce62169cf4d7698ac8a61f71483
Ben Shelton <bks-44@hotmail.com>
2022-08-24 15:36:12 -0500

    phase 2

--- Program.cs
+++ Program.cs
@@ -43,7 +43,11 @@ namespace Quest
             int maxAwesomeness = 100;
 
             // Make a new "Adventurer" object using the "Adventurer" class
-            Adventurer theAdventurer = new Adventurer("Jack");
+            Console.Write("Hello Adventurer! Please enter your name: ");
+            string userName = Console.ReadLine();
+
+            Adventurer theAdventurer = new Adventurer(userName);
+            Console.WriteLine($"Welcome, {theAdventurer.Name}!");
 
             // A list of challenges for the Adventurer to complete
             // Note we can use the List class here because have the line "using System.Collections.Generic;" at the top of the file.
