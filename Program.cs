d1c8641283d21a87d0d4449ae46fa05d14c3cd9f
Ben Shelton <bks-44@hotmail.com>
2022-08-24 15:08:14 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace adventurers_quest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
